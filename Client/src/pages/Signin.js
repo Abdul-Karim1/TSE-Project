00b126147baf34aea5e70f94e93f92b934281f58
Fatima060-08 <122373440+Fatima060-08@users.noreply.github.com>
2023-05-24 21:43:01 +0500

    Comments added to Signin.js

--- Client/src/pages/Signin.js
+++ Client/src/pages/Signin.js
@@ -1,26 +1,31 @@
-import React, { Component } from "react";
-import "../styles/Contact.css";
+import React, { Component } from 'react';
+import '../styles/Contact.css';
 
 class Signin extends Component {
-  constructor(props) { //data response will come to this constructor as prop
+  /*will be used to handle sign in functionality, The class has a constructor that initializes the state with 
+  properties for email and password. It also has a handleSubmit function that handles the form submission and 
+  makes a POST request to a local server for user authentication*/
+  constructor(props) {
+    //data response will come to this constructor as prop
     super(props);
     this.state = {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleSubmit(e) {
+    // handles the form submission and makes a POST request to a local server for user authentication
     e.preventDefault();
     const { email, password } = this.state;
     console.log(email, password);
-    fetch("http://localhost:5000/login-user", {
-      method: "POST",
+    fetch('http://localhost:5000/login-user', {
+      method: 'POST',
       crossDomain: true,
       headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "Access-Control-Allow-Origin": "*",
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Access-Control-Allow-Origin': '*',
       },
       body: JSON.stringify({
         email,
@@ -29,30 +34,42 @@ class Signin extends Component {
     })
       .then((res) => res.json())
       .then((data) => {
-        if (data.status === "ok") {
-          alert("Signed In Successfully");
+        if (data.status === 'ok') {
+          /*if data added by user matches the data stored in the database then user will be
+              able to successfully signin, otherwise error will occur*/
+          alert('Signed In Successfully');
           //This line tells us that we have logged in (On Local Storage)
-          window.localStorage.setItem("token", data.data);
-          window.location.href = "./menu";
-        }
-        else {
-          alert("Invalid Email Or Password");
+          window.localStorage.setItem('token', data.data);
+          window.location.href = './menu';
+        } else {
+          alert('Invalid Email Or Password');
         }
       });
   }
   render() {
     return (
-      <div className="contact center" >
-
+      <div className="contact center">
         <div className="rightSide">
           <h1>Welcome Back! Sign In To Continue</h1>
 
           <form onSubmit={this.handleSubmit}>
             <label htmlFor="email">Your Email</label>
-            <input name="email" placeholder="Enter email..." type="email" required onChange={(e) => this.setState({ email: e.target.value })} />
+            <input
+              name="email"
+              placeholder="Enter email..."
+              type="email"
+              required
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
 
             <label htmlFor="password">Enter Password</label>
-            <input name="password" placeholder="Enter Password..." type="Password" required onChange={(e) => this.setState({ password: e.target.value })} />
+            <input
+              name="password"
+              placeholder="Enter Password..."
+              type="Password"
+              required
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
 
             <button type="submit">Sign In</button>
           </form>
@@ -61,7 +78,5 @@ class Signin extends Component {
     );
   }
 }
-
-
 
 export default Signin;
